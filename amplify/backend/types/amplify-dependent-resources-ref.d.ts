3605914433b97696a2207cd784b2982a322934a4
jdoubled05 <76123876+jdoubled05@users.noreply.github.com>
2023-03-01 17:10:35 -0500

    amplify reset

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,17 @@
+<<<<<<< Updated upstream
 export type AmplifyDependentResourcesAttributes = {}
+=======
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "steprxa46f13ad": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
+>>>>>>> Stashed changes
